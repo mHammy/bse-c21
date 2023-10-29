0d01c5792f273a4806776cea45363ea3fcf7046c
mHammy <133612424+mHammy@users.noreply.github.com>
2023-10-29 14:06:01 -0400

    Updated some of the code required in the pages and the App.jsx, not yet completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,23 @@
+import React from 'react';
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 import Navbar from './components/Navbar';
 
+// Initialize Apollo Client
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache()
+});
+
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
